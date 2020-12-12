7249127eccb7583d9909402aca5968ec51cfecbf
Andre Braunersreuther <andre.braunersreuther@smarteye.se>
2020-12-12 14:30:54 +0100

    Day 11

---
+++ day_11/11_1.py
@@ -0,0 +1,78 @@
+
+def neighbour_coords(x, y, w, h):
+    coords = []
+    t_x = 0
+    t_y = 0
+    for _x in range(-1, 2):
+        for _y in range(-1, 2):
+            t_x = x + _x
+            t_y = y + _y
+
+            if t_x == x and t_y == y:
+                continue
+            if (t_x >= 0 and t_y >= 0) and (t_x < w and t_y < h):
+                coords.append((t_x, t_y))
+
+    return coords
+
+def neighbours(area, x, y):
+    width = len(area[0])
+    height = len(area)
+    coords = neighbour_coords(x, y, width, height)
+
+    count = 0
+    for co in coords:
+        x = co[0]
+        y = co[1]
+        if area[y][x] == "#":
+            count += 1
+    return count
+
+def diff(one, another):
+    d = 0
+    for r, row in enumerate(one):
+        for c, col in enumerate(row):
+            if not one[r][c] == another[r][c]:
+                d += 1
+    return d
+
+def print_pretty(area):
+    for row in area:
+        print(row)
+
+def count_occupied(area):
+    count = 0
+    for r, row in enumerate(area):
+        for c, col in enumerate(row):
+            if area[r][c] == "#":
+                count += 1
+    return count
+
+if __name__ == "__main__":
+    with open("test_11_1.in") as f:
+        area = [x.strip() for x in f.readlines()]
+
+    while True:
+        next_area = []
+
+        for y, row in enumerate(area):
+            new_col = ''
+            for x, col in enumerate(row):
+                if area[y][x] == "L" and neighbours(area, x, y) == 0:
+                    new_col += "#"
+                elif area[y][x] == "#" and neighbours(area, x, y) >= 4:
+                    new_col += "L"
+                else:
+                    new_col += area[y][x]
+
+            next_area.append(new_col)
+
+        print_pretty(next_area)
+
+        print("")
+
+        if diff(area, next_area) == 0:
+            print(count_occupied(area))
+            break
+        area = next_area.copy()
+
