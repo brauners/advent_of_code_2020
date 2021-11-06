c76610b8234db4ad6403e2f5841f012341af50cf
AndreB <brauners@hm.edu>
2021-11-06 17:06:12 +0100

    add template folder

---
+++ python/template/main.py
@@ -0,0 +1,10 @@
+
+def func(arg):
+    pass
+
+if __name__ == "__main__":
+    input_filename = "day_00/input"
+    input_filename = "day_00/input_debug"
+    with open(input_filename) as fp:
+        input_data = fp.read().split()
+ 
